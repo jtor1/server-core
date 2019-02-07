ade06a0fc1ae4c76c904225a0e0d1fbd8595bc73
Michael Corrigan <mcorrigan89@gmail.com>
2019-02-07 14:10:17 -0800

    export createContext

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { Context } from './apollo.context'
+import { Context, createContext } from './apollo.context'
 import { createApolloServer } from './apollo.server'
 import { IServer, Server } from './server'
 import { initApp } from './server.init';
 
 export {
   Context,
+  createContext,
   createApolloServer,
   IServer,
   Server,
