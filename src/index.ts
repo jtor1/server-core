3b1e03eec37cbbe22f421de30ed46edae8b39fe8
Michael Corrigan <mcorrigan89@gmail.com>
2019-02-07 20:13:28 -0800

    move types to deps

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { IServer, Server } from './server/server'
 import { initApp } from './server/server.init';
 
 import { ControllerTemplate } from './templates/controller.template';
-import { EntityModelTemplate } from './templates/model.template';
+import { EntityModelTemplate, ModelTemplate } from './templates/model.template';
 import { Template } from './templates/entity.template';
 
 export {
@@ -15,6 +15,7 @@ export {
   Server,
   initApp,
   ControllerTemplate,
+  ModelTemplate,
   EntityModelTemplate,
   Template
 }
