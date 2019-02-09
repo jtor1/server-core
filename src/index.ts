937a2a7afe6880814e9b07162a3b6bc7b7fb8d31
Michael Corrigan <mcorrigan89@gmail.com>
2019-02-08 16:40:10 -0800

    export auth middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Context, createContext } from './server/apollo.context'
 import { createApolloServer } from './server/apollo.server'
 import { IServer, Server } from './server/server'
 import { initApp } from './server/server.init';
+import { tokenCheck } from './authentication/token.check';
+import { TokenConfig, verifyAll } from './authentication/verify.token';
 
 import { ControllerTemplate } from './templates/controller.template';
 import { EntityModelTemplate, ModelTemplate } from './templates/model.template';
@@ -17,5 +19,8 @@ export {
   ControllerTemplate,
   ModelTemplate,
   EntityModelTemplate,
-  Template
+  Template,
+  tokenCheck,
+  TokenConfig,
+  verifyAll
 }
