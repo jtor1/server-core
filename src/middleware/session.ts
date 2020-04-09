d9fbb1285a6237c3c254afd48607054d6e1ef5b2
johnroy <jroy@jr.local>
2020-04-09 14:44:35 -0700

    Add console logging

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -16,8 +16,10 @@ export default function (maybeOptions?: object): RequestHandler {
     const reqAsAny: any = <any>req;
 
     let sessionId = (cookie.parse(headers['cookie'] || '') || {})[SESSION_COOKIE_NAME]; // too much foo?
+console.log('SSSSSS session id = ', sessionId);
     if (!sessionId) {
       sessionId = _makeSessionId();
+console.log('SSSSSS not found');
       res.setHeader('Set-Cookie', cookie.serialize('name', sessionId, {
 //        httpOnly: true, .. what? TODO
         maxAge: 60 * 60 * 24 * 7 // 1 week TODO
@@ -25,6 +27,7 @@ export default function (maybeOptions?: object): RequestHandler {
     }
 
     reqAsAny[SESSION_COOKIE_NAME] = sessionId;   // is this right (tix: "held as Request#sessionId")
+console.log('SSSSSS setting session id', sessionId);
 
     next();
 //        next(err); TODO .. reminder of how to throw and error
