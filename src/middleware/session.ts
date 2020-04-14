01bc65b7b87dea1e65420fa89c4ae78cd90bc05f
johnroy <jroy@jr.local>
2020-04-14 16:58:25 -0700

    Cleanup a version to facilitate early PR discussion

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -1,4 +1,3 @@
-//import bodyParser, { OptionsText } from 'body-parser'; 
 import cookie from 'cookie';
 import { Request, Response, NextFunction, RequestHandler } from 'express'
 
@@ -13,37 +12,25 @@ function _makeSessionId(): string {
     return Math.random().toString(36).substring(2, 15);
 }
 
-//export function bodyParserGraphql(maybeOptions?: OptionsText): RequestHandler {
 export default function (maybeOptions?: object): RequestHandler {
-//  return function bodyParserGraphql(req: Request, res: Response, next: NextFunction): void {
   return function sessionId(req: Request, res: Response, next: NextFunction): void {
     const { headers } = req;
     const reqAsAny: any = <any>req;
 
     let sessionId = (cookie.parse(headers['cookie'] || '') || {})[SESSION_COOKIE_NAME]; // too much foo?
-console.log('SSSSSS session id = ', sessionId);
     if (!sessionId) {
       sessionId = _makeSessionId();
-console.log('SSSSSS not found');
       res.setHeader('Set-Cookie', cookie.serialize(SESSION_COOKIE_NAME, sessionId, {
-//        httpOnly: true, .. what? TODO
         path: '/',
         domain: '.withjoy.com',      // TODO set/use as a constant somehow?
         httpOnly: true,
         maxAge: 60 * 60 * 24 * 7, // 1 week TODO
-sameSite: 'none',
-//expires: new Date('Tue, 13 Apr 2021 23:20:42 GMT')
+        sameSite: 'none',
       }));
     }
-else { 
-console.log('SSSSSS is found', sessionId);
-}
 
     reqAsAny[SESSION_COOKIE_NAME] = sessionId;   // is this right (tix: "held as Request#sessionId")
-console.log('SSSSSS setting session id', sessionId);
 
     next();
-//        next(err); TODO .. reminder of how to throw and error
-//        return;
   }
 }
