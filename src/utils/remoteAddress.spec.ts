24ba99a7bd2fa81a17087cfb5fe823024868abbe
Dan Foley <dan@withjoy.com>
2021-05-24 18:08:08 -0700

    CHORE derive the originating Client IP from an 'x-forwarded-for' with multiple values

--- src/utils/remoteAddress.spec.ts
+++ src/utils/remoteAddress.spec.ts
@@ -1,3 +1,4 @@
+import { Socket } from 'net';
 import { createRequest } from 'node-mocks-http';
 
 import {
@@ -18,13 +19,54 @@ describe('utils/remoteAddress', () => {
       expect(deriveRemoteAddress(req)).toBe(REMOTE_ADDRESS);
     });
 
-    it('derives from multiple "x-forwarded-for" headers', () => {
+    it('derives the originating Client IP from multiple "x-forwarded-for" headers', () => {
       const req = createRequest({
         headers: {
-          'x-forwarded-for': <any>[ REMOTE_ADDRESS, 'IGNORED' ],
+          'x-forwarded-for': (<unknown>[ REMOTE_ADDRESS, 'IGNORED' ] as string),
         },
       });
       expect(deriveRemoteAddress(req)).toBe(REMOTE_ADDRESS);
+
+      // it('trusts that the headers were well-formed')
+      //   ... which in this case, they were *not*
+      expect(deriveRemoteAddress( createRequest({
+        headers: {
+          'x-forwarded-for': (<unknown>[ '', 'IGNORED' ] as string),
+        },
+      }) )).toBeUndefined();
+    });
+
+    it('derives from an "x-forwarded-for" header with multiple values', () => {
+      const req = createRequest({
+        headers: {
+          'x-forwarded-for': (<unknown>[ `${ REMOTE_ADDRESS }, IGNORED`, 'IGNORED' ] as string),
+        },
+      });
+      expect(deriveRemoteAddress(req)).toBe(REMOTE_ADDRESS);
+
+      // it('trusts that the headers were well-formed')
+      //   ... which in these cases, they were *not*
+      expect(deriveRemoteAddress( createRequest({
+        headers: {
+          'x-forwarded-for': ', IGNORED',
+        },
+      }) )).toBeUndefined();
+
+      expect(deriveRemoteAddress( createRequest({
+        headers: {
+          'x-forwarded-for': (<unknown>[ , 'IGNORED' ] as string),
+        },
+      }) )).toBeUndefined();
+    });
+
+    it('does not derive the Socket#remoteAddress', () => {
+      // for reasons explained in the method
+      const req = createRequest({
+        connection: ({ remoteAddress: REMOTE_ADDRESS } as Socket),
+      });
+      expect(req.connection.remoteAddress).toBe(REMOTE_ADDRESS);
+
+      expect(deriveRemoteAddress(req)).toBeUndefined();
     });
 
     it('is tolerant of unexpected scenarios', () => {
@@ -33,12 +75,11 @@ describe('utils/remoteAddress', () => {
       expect(deriveRemoteAddress( <any>{} )).toBeUndefined();
       expect(deriveRemoteAddress( createRequest() )).toBeUndefined();
 
-      const req = createRequest({
+      expect(deriveRemoteAddress( createRequest({
         headers: {
           'x-forwarded-for': '',
         },
-      });
-      expect(deriveRemoteAddress(req)).toBeUndefined();
+      }) )).toBeUndefined();
     });
   });
 });
--- src/utils/remoteAddress.ts
+++ src/utils/remoteAddress.ts
@@ -1,13 +1,37 @@
 import { Request } from 'express';
+import { first } from 'lodash';
 
+/**
+ * @returns {String} the IP address of the originating Client, or `undefined`
+ */
 export function deriveRemoteAddress(req: Request | null | undefined): string | undefined {
-  // we tried using `forwarded`, but it derives *local* addresses as well;
+  // inspired by '@withjoy/telemetry' + `deriveIPFromRequest`
+  //   in that it respects 'x-forwarded-for', and grabs the "farthest" IP from it
+  //   but ... we can't use it directly because it derives `Socket#remoteAddress`
+  //   (see below)
+  // we previously tried using `forwarded`, but it derives *local* addresses as well;
   //   that does not suit our needs;
   //   for example, it would be VERY BAD for us to `limitd` / rate limiting on '::ffff:127.0.0.1'
+  //   or the IP of the immediate upstream load balancer
   //   we only care about true forwarding, via standard header
-  const derived = req?.headers?.['x-forwarded-for'];
-  return (Array.isArray(derived)
-    ? derived[0]
-    : derived
-  ) || undefined;
+
+  // https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
+  //   4.2 Message Headers
+  //   "It MUST be possible to combine the multiple header fields into one "field-name: field-value" pair,
+  //    without changing the semantics of the message, by appending each subsequent field-value to the first,
+  //    each separated by a comma."
+  const headers = req?.headers?.['x-forwarded-for'];
+  const header: string | undefined = (Array.isArray(headers)
+    ? headers[0]
+    : headers
+  );
+  if (! header) {
+    return undefined;
+  }
+
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For
+  //   "the right-most IP address is the IP address of the most recent proxy
+  //    and the left-most IP address is the IP address of the originating client."
+  const addresses = header.split(/\s*,\s*/);
+  return first(addresses) || undefined;
 }
